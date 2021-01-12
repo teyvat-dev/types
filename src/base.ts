a2c49870ba1257f797376f4788c4e0e8f3d8ab92
Drew Hutton <drew.business2308@gmail.com>
2021-01-12 17:38:14 +1030

    wip feat: Extra types for JSON values

--- src/base.ts
+++ src/base.ts
@@ -4,7 +4,6 @@ export {
   Character,
   CharacterAscension,
   CharacterAscensionMaterial,
-  CharacterProfile,
   CommonAscensionMaterial,
   CommonMaterial,
   Consumeable,
@@ -24,4 +23,86 @@ export {
   WeaponAscension,
   WeaponAscensionMaterial,
   WeaponEnhancementMaterial,
-} from "./types/client";
+} from './types/client';
+
+// Special Typed JSON exports
+import type {
+  Character as CharacterBase,
+  CharacterProfile as CharacterProfileBase,
+} from './types/client';
+
+type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
+
+interface CharacterProfileModified extends CharacterProfileBase {
+  /**
+   * JSON object containing the images of the character
+   *
+   * @real-type JSON
+   */
+  images: {
+    /**
+     * Portrait image of the Character
+     *
+     * @real-type String
+     */
+    portrait: string;
+    /**
+     * Card image of the Character
+     *
+     * @real-type String
+     */
+    card: string;
+    /**
+     * inGame image of the Character
+     *
+     * @real-type String
+     */
+    inGame: string;
+  };
+  /**
+   * JSON object containing the characters stories
+   *
+   * @real-type JSON
+   */
+  story: {
+    /**
+     * Story with the Key being the name of the story
+     *
+     * @real-type String
+     */
+    [key: string]: string;
+  };
+  /**
+   * JSON object containing voice actors for the character
+   *
+   * @real-type JSON
+   */
+  voiceActor: {
+    /**
+     * English Voice Actor
+     *
+     * @real-type String
+     */
+    en: string;
+    /**
+     * Japanese Voice Actor
+     *
+     * @real-type String
+     */
+    jp: string;
+    /**
+     * Korean Voice Actor
+     *
+     * @real-type String
+     */
+    kr: string;
+    /**
+     * Chinese Voice Actor
+     *
+     * @real-type String
+     */
+    cn: string;
+  };
+}
+
+export type CharacterProfile = Expand<CharacterProfileModified>;
